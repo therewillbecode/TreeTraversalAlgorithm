95c0a95acbd01a5ac4acad85e439df33f74fb0d1
TomChambrier <tomw08@gmail.com>
2015-10-29 14:09:04 +0000

    addition calculation works

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
+module.exports.conv = conv;
+module.exports.calculate = calculate;
+
+
+function calculate(a, b){
+
+return conv(a) + conv(b)
+
+}
+
+
 function conv(x){
+    var cur = Math.pow(2, (x.length));
 
     return Array.prototype.map.call(x, function(val, index){
-        console.log('index:' + (x.length-index) + ' val : ' + (index *2));
+        cur /= 2;
+
+     //   console.log('index:' + (x.length-index) + ' val : ' + (Math.pow(index, 2)));
         if (val == 1 && index == x.length -1){
             return (1);
         }
         if (val == 1 && index == x.length -2) {
+            cur =2;
             return (2);
         }
         if (val == 1){
             pos = x.length - (index + 1);
-            console.log(pos);
+          //  console.log(pos);
             // return counter;
             //'' recursively  double prev val
-            return ((Math.pow(pos, 2)));
+            return (cur);
         }
         else{
             return 0;
         }
-    });
-    // .reduce(function(a,b){return a + b})
+    })
+    .reduce(function(a,b){return a + b})
 }
 
-console.log(conv("111111"));
+console.log(conv("100"));
--- test/test.js
+++ test/test.js
@@ -2,21 +2,36 @@ var index = require("../index");
 var chai = require('chai');
 var expect = require('chai').expect;
 
-describe('a basic test', function () {
-    it('should pass when everything is okay', function () {
-        expect(index.add()).to.equal('333');
+
+
+describe('should convert sum binary and output decimal', function () {
+    it('"0","0" should give 0', function () {
+        expect(index.calculate("0","0")).to.equal(0);
+    });
+
+    it('"1","1" should give 2', function () {
+        expect(index.calculate("1","1")).to.equal(2);
+    });
+
+    it('"10","10" should give 4', function () {
+        expect(index.calculate("10","10")).to.equal(4);
+    });
+
+    it('"101","100" should give 9', function () {
+        expect(index.calculate("101","100")).to.equal(9);
+    });
+
+    it('"1010101","1010101" should give 142', function () {
+        expect(index.calculate("1010101","1010101")).to.equal(170);
     });
 });
 
-describe('convert binary', function () {
-    it('should convert binary to hexadecimal', function () {
+describe('should convert binary to decimal', function () {
+    it('should convert binary to decimal', function () {
         expect(index.conv("0")).to.equal(0);
-
     });
-});
 
-describe('convert binary', function () {
-    it('should convert binary to hexadecimal', function () {
+    it('should convert binary to decimal', function () {
         expect(index.conv("1")).to.equal(1);
     });
 
@@ -24,16 +39,11 @@ describe('convert binary', function () {
         expect(index.conv("100")).to.equal(4);
     });
 
-    it('1001 should equal 9', function () {
-        expect(index.conv("1001")).to.equal(9);
-    });
-
-    it('10011 should equal 19', function () {
-        expect(index.conv("10011")).to.equal(19);
-    });
-
     it('11111 should equal 19', function () {
-        expect(index.conv("1111111")).to.equal(35);
+        expect(index.conv("1111111")).to.equal(127);
     });
 
+    it('01010101001110011100000 should equal 2792672', function () {
+        expect(index.conv("01010101001110011100000")).to.equal(2792672);
+    });
 });
