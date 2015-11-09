7ef113086539abce1fb09ffa2124e09d9ef36485
Tom <tomw08@gmail.com>
2015-11-09 19:57:03 +0000

    someawht traverss

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-module.exports.getRoute = getRoute;
-module.exports.node = Node;
-module.exports = {
-    getroute : getRoute,
-    node : Node
-};
+
 
 //a constructor for adding a node to the tree
 var Node = function(value, edges){
@@ -17,16 +12,53 @@ var Node = function(value, edges){
 //an object containing all the nodes in the tree
 var nodes = {};
 
-nodes['1'] = new Node(1, 2);
-nodes['2'] = new Node(2, 3);
+var
+    f = Node("f", []),
+    g = Node("g", []),
+    h = Node("h", []),
+    e = Node("e", [g]),
+    d = Node("d", [f, g, h]),
+    a = Node("a", [d]),
+    b = Node("b", [d, e]),
+    c = Node("c", [e, h]);
+
 //a and b are of type Node
 //find out if there is a route from a to b
-var getRoute = function(a,b){
+var getRoute = function(a, b){
+    b = b.value
 
-
+    // set off recursion train for each edge of starting node
+    edges =  getEdges(a)
+    console.log(edges)
+    for (var prop in edges){
+        if(edges[prop]==b){return true}
+        console.log(edges[prop])
+        console.log(getEdges(edges[prop]))
+    }
 
 
 };
 
-console.log(nodes);
+// returns True if b is edge of a
+// else returns list of edges
+// if no edges returns false
+function getEdges(a){
+var edgelist = [];
+// get edges for given Node
+    for (var prop in a.edges){
+            theEdge = a.edges[prop].value;
+            edgelist.push(theEdge);
+            if (theEdge == b){ return true}
+        }
+
+    if(edgelist.length == 0){   // no edges
+        return false
+    }
+    else{
+        return edgelist;
+    }
+}
+
+console.log(getRoute(d,f));
+//console.log(nodes);
 
