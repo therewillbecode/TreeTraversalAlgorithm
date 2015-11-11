c2bf14cbb42a047606412e55c62c663f87bb54b3
Tom <tomw08@gmail.com>
2015-11-11 13:42:32 +0000

    yay

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 //a constructor for adding a node to the tree
 var Node = function(value, edges){
     nodes[value] = {
@@ -11,6 +12,7 @@ var Node = function(value, edges){
 //an object containing all the nodes in the tree
 var nodes = {};
 
+
 var
     f = Node("f", []),
     g = Node("g", []),
@@ -20,50 +22,32 @@ var
     a = Node("a", [d]),
     b = Node("b", [d, e]),
     c = Node("c", [e, h]);
-    j = Node("j", [c]);
-    w = Node("w", [j, g])
-    p = Node("p", [w])
+
 
 //a and b ar
 //find out if there is a route from a to b
 var getRoute = function(a, xx){
+
+
+
+    //console.log(nodes);
     //get edges
     function getEdges(a){
+        visited.push(a.value);
         var edgelist = [];
-        //console.log(a)
 
         for (var prop in a.edges){
-            edgelist.push(a.edges[prop])
+            edgelist.push(a.edges[prop]);
 
-            try {
+          //  try {
                 if (a.edges[prop].value == xx.value) {
-                    found = true
-                }
+                    found = true;
+         //       }
             }
-            catch(e){console.log(e)}
+          //  catch(e){console.log(e)}
         }
         if(edgelist.length == 0){ return 'undefined'}
         return edgelist;
     }
-
+    var visited = [];    // stores value of visited nodes
     found = false;
-
-    edges = getEdges(a);    // get edges for first node
-
-    neighbourCalls(edges);
-
-    function neighbourCalls(edgelist) {
-        console.log(edgelist);
-        for (var prop in edgelist) {
-            edgeSet = getEdges(edgelist[prop]);      // gets edges for given neighbour
-
-            if(typeof edgeSet[0] == 'object'){     // if neighbours exist for these neighbours then call recursively
-                neighbourCalls(edgeSet)
-            }
-        }
-    }
-    return found
-};
-
-
-console.log(getRoute(p ,h) + '4444');
